7ea6647d2a6ec6e0c46bdcde32cb5b63f797d4f8
Roman Nekrasov <semeonsemenchenko@yandex.ru>
2022-05-12 03:22:28 +0300

    Sprint_4 commit

--- src/main/java/Praktikum.java
+++ src/main/java/Praktikum.java
@@ -5,13 +5,6 @@ public class Praktikum {
         Account account = new Account(name);
         boolean result = account.checkNameToEmboss();
         System.out.println(result);
-
-
-        /*
-             В этом методе заложи логику работы с классом Account.
-             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
-             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
-         */
     }
 
 }
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,126 +1,45 @@
-import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class CardTest {
 
+    private String name;
+    private boolean expected;
+
+    public CardTest(String name, boolean expected) {
+        this.name = name;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: {0} {1}")
+    public static Object[][] dataForTest() {
+        return new Object[][]{
+                {"Иван Иванов", true},
+                {"Иазфывапролдйцу gд", true},
+                {"Иазфывапролдйцу gдq", true},
+                {"И з", true},
+                {"Иa p", true},
+                {"И ", false},
+                {"Иазфывапролдйцу gдqq", false},
+                {"Иван  Иванов", false},
+                {" ИванИванов", false},
+                {"ИванИванов ", false},
+                {"И и и", false},
+                {"Иаз", false},
+                {null, false},
+                {"", false},
+        };
+    }
+
     @Test
-    @DisplayName("Проверка печати имени владельца карты, соответствующего всем требованиям")
-    public void printCardholdernameWithAllRequirements(){
-        boolean expected = true;
-        String name = "Иван Иванов";
+    public void printCardholderNamePrint() {
         Account account = new Account(name);
         boolean actual = account.checkNameToEmboss();
         assertEquals(expected, actual);
     }
 
-    @Test
-    @DisplayName("Проверка граничного значения из одного символа и пробела")
-    public void printCardholdernameOneSymbolAndSpace(){
-        boolean expected = false;
-        String name = "И ";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка граничного значения имени держателя карты из двух символов и пробела")
-    public void printCardholdernameTwoSymbolsAndSpace(){
-        boolean expected = true;
-        String name = "И з";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка граничного значения имени держателя карты из трёх символов и пробела")
-    public void printCardholdernameThreeSymbolsAndSpace(){
-        boolean expected = true;
-        String name = "Иa p";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка граничного значения имени держателя карты из семнадцати символов и пробела")
-    public void printCardholdernameSeventeenSymbolsAndSpace(){
-        boolean expected = true;
-        String name = "Иазфывапролдйцу gд";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка граничного значения имени держателя карты из восемнадцати символов и пробела")
-    public void printCardholdernameEighteenSymbolsAndSpace() {
-        boolean expected = true;
-        String name = "Иазфывапролдйцу gдq";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка граничного значения имени держателя карты из девятнадцати символов и пробела")
-    public void printCardholdernameNineteenSymbolsAndSpace() {
-        boolean expected = false;
-        String name = "Иазфывапролдйцу gдqq";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка имени держателя карты содержащего два пробела подряд")
-    public void printCardholdernameTwoSpaces(){
-        boolean expected = false;
-        String name = "Иван  Иванов";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка имени держателя карты содержащего пробел в начале")
-    public void printCardholdernameWithSpaceInTheBeginning() {
-        boolean expected = false;
-        String name = " ИванИванов";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка имени держателя карты содержащего пробел в конце")
-    public void printCardholdernameWithSpaceInTheEnd() {
-        boolean expected = false;
-        String name = "ИванИванов ";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка имени держателя карты содержащего пробелы, чередующиеся с символами")
-    public void printCardholdernameSymbolAndSpace(){
-        boolean expected = false;
-        String name = "И и и";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Проверка имени держателя карты не содержащего пробелы")
-    public void printCardholdernameWithoutSpace(){
-        boolean expected = false;
-        String name = "Иаз";
-        Account account = new Account(name);
-        boolean actual = account.checkNameToEmboss();
-        assertEquals(expected, actual);
-    }
 }
